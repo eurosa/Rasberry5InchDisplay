79872ff935bf81d94bc0166f690f0f2e7e8151d5
rnjn <radhasonk.phy@gmail.com>
2022-03-19 04:22:23 -0700

    ok

---
+++ config-display.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# > means overrite everything
+# >> means add new line to existing
+
+echo '
+max_usb_current=1
+hdmi_group=2
+hdmi_mode=87
+hdmi_cvt 800 480 60 6 0 0 0
+hdmi_drive=1' >> /boot/config.txt
+echo '
+network={
+    ssid="FTTH-5G"
+    psk="Digiline@123"
+    key_mgmt=WPA-PSK
+}' >> /etc/wpa_supplicant/wpa_supplicant.conf
+
+cd /warmer/LCD-show/
+chmod +x LCD5-show
+./LCD5-show
+
+sudo reboot
