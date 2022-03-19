3d051d1ad73170c20f882fe5c13a59015ca85f1e
rnjn <radhasonk.phy@gmail.com>
2022-03-19 04:22:03 -0700

    ok

--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/sh 
 sudo apt-get update
 sudo python3 -m pip install self
 sudo apt install rename
-sudo apt-get purge --remove plymouth
+sudo sed -i '/SearchPattern/[Seat:*]' /etc/lightdm/lightdm.conf
+#sudo nano /etc/lightdm/lightdm.conf
+#xserver-command=X -s 0 dpms
+# sudo apt-get purge --remove plymouth
 sudo apt-get install python3-self
 sudo apt-get install pyqt5.Qtsql
 sudo apt-get install python3-pyqt5
@@ -12,10 +15,19 @@ sudo update-rc.d ssh defaults
 sudo systemctl enable ssh.socket
 sudo systemctl enable ssh.service
 sudo service ssh start
-sudo service ssh status
-git clone https://github.com/waveshare/LCD-show.git
-cd LCD-show/
-chmod +x LCD5-show
-./LCD5-show
+#sudo service ssh status 
+echo '# Disable the rainbow splash screen
+disable_splash=1 
+# Set the bootloader delay to 0 seconds. The default is 1s if not specified.
+boot_delay=0
+
+# Overclock the raspberry pi. This voids its warranty. Make sure you have a good power supply.
+force_turbo=1' >> /boot/config.txt
+sudo reboot
+#git clone https://github.com/waveshare/LCD-show.git
+#cd LCD-show/
+#chmod +x LCD5-show
+#./LCD5-show
+
 
 
